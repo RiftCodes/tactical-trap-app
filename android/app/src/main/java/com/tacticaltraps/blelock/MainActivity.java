346a6fe4829ae8467f67fc43d52d6b34f84ead44
Faizan Elahi <89972827+faizanMayo786@users.noreply.github.com>
2025-07-18 23:58:41 +0500

    1.5.1

--- android/app/src/main/java/com/tacticaltraps/blelock/MainActivity.java
+++ android/app/src/main/java/com/tacticaltraps/blelock/MainActivity.java
@@ -1,5 +1,18 @@
 package com.tacticaltraps.bluetooth.lock_2;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable edge-to-edge display for Android 15+ compatibility
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+    }
+}
